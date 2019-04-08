aa8a34b8a82bf455af89912e652bf842d3112ccf
Etka Yapar <etka.yapar@gmail.com>
2019-04-08 17:36:26 +0300

    initialize new repo

---
+++ bamRefine.py
@@ -0,0 +1,27 @@
+from sys import stdin
+
+
+def isTransition(ref, bamR):
+
+    table = {
+        'a': 'g',
+        't': 'c',
+        'g': 'a',
+        'c': 't'}
+
+    if table[ref] == bamR:
+        return True
+    else:
+        return False
+
+def parseBam(bamLine):
+    bamLine = bamLine.strip().split()
+    pos = bamLine[3]
+    seq = bamLine[9]
+
+    return (bamLine, pos, seq)
+
+for bamL in stdin:
+    bamL, seq, pos = parseBam(bamL)
+    # Find if seq includes SNPs
+
