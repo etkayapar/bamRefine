9d523157813ec650d23265bac793f65a5dbeef25
Etka Yapar <etka.yapar@gmail.com>
2019-04-09 09:22:00 +0300

    think
    Former-commit-id: 416cb3a69af30fbad4c8b396a72d36a0ad23b63a

--- bamRefine.py
+++ bamRefine.py
@@ -23,9 +23,31 @@ def parseBam(bamLine):
 
 # Parse snp catalogue
 
+def parseSNPs(fName):
+    snpF = open(fName)
+    # curC = 'chr1'
+    chrm = 'chr1'
+    tmpS = []
+    snps = {}
+
+    for snp in snpF:
+        snp = snp.strip().split()
+        curC = snp[0]
+        if curC != chrm:
+            snps[chrm] = tmpS
+            tmpS = []
+            tmpS.append(snp[1:])
+            chrm = curC
+        tmpS.append(snp[1:])
+
+    snpF.close()
+    return snps
 
 
-for bamL in stdin:
-    bamL, seq, pos = parseBam(bamL)
-    # Find if seq includes SNPs
+
+
+
+# for bamL in stdin:
+#     bamL, seq, pos = parseBam(bamL)
+#     # Find if seq includes SNPs
 
