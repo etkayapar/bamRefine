682aebdb4c709bef2b3b3e26c2d98016c60281a4
Etka Yapar <etka.yapar@gmail.com>
2019-04-18 14:56:54 +0300

    first implementation

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from bamRefine import *
 import pysam
 
@@ -5,20 +6,22 @@ snps = parseSNPs('chr_pos_ref_alt_1240K.all.snp')
 # snps = list(snps.values())
 # snps = snps[:3000]
 
+ouName = sys.argv[1] + ".bam"
+
 inBAM = pysam.AlignmentFile('../deneme.bam', 'rb')
-ouBAM = pysam.AlignmentFile('../denemeout.bam', 'wb', template=inBAM)
+ouBAM = pysam.AlignmentFile(ouName, 'wb', template=inBAM)
 
 #fReads = snpMask(snps, inBAM)
 
 #filterBAM(inBAM, ouBAM, fReads)
 
 
-for read in inBAM.fetch():
+for read in inBAM.fetch(sys.argv[1]):
     bamL = read.to_dict()
     mask, m_pos = flagReads(snps, bamL)
     if mask == True:
         for p in m_pos:
-            print('in read %s, position %d' % (bamL['name'], p))
+            # print('in read %s, position %d' % (bamL['name'], p))
             t = list(bamL['seq']) ; t[p] = 'N' ; t = "".join(t)
             bamL['seq'] = t
         # bamLine --> pysam.AlignedSegment
@@ -27,17 +30,6 @@ for read in inBAM.fetch():
     else:
         bamL = pysam.AlignedSegment.from_dict(bamL, inBAM.header)
         ouBAM.write(bamL)
-
-
-
-
-# with open('maskedReads.txt', 'w') as f:
-#     for r in fReads.keys():
-#         f.write(r + "\n")
-
-
-
-
 
 inBAM.close()
 ouBAM.close()
