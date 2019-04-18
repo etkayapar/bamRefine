9e0c7dc00843d2c485f0a0225ad12884890d3200
Etka Yapar <etka.yapar@gmail.com>
2019-04-18 14:56:54 +0300

    first implementation
    Former-commit-id: 9baa749333275caf3384a9934a0e51dbe80f1f3f

--- main_wrapper.py
+++ main_wrapper.py
@@ -1,10 +1,26 @@
 import pysam
+from subprocess import Popen
 
 chrms = range(1,25)
 
+procs = []
 for c in chrms:
     cmd = "python3 main.py " + c
-    os.system(cmd)
+    p = Popen([cmd], shell = True)
+    procs.append(p)
+
+while len([x.poll() for x in procs if x.poll() != None]) != len(procs):
+    continue
+
+print("Merging BAM files...")
+
 
 
 merge_cmd = "samtools merge -b " + toMerge + " -O BAM merged_out.bam"
+
+merging = Popen([merge_cmd], shell = True)
+
+while merging.poll() == None:
+    continue
+
+print("Finished merging.\nProgram executed successfully")
