33d59edb8bfe0b785ebf2b77f7bc3fb44d752841
Etka Yapar <etka.yapar@gmail.com>
2019-05-07 10:52:25 +0300

    migrate the line by line parsing function from main to module file
    Former-commit-id: ea1d703422010ea9cb47728e9e5fb3828dd630d8

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
-from bamRefine import *
-import pysam
+from bamRefine_cy import *
+# import pysam
 
 
 inName = sys.argv[1]
@@ -17,25 +17,4 @@ ouName = contig + ".bam"
 inBAM = pysam.AlignmentFile(inName, 'rb')
 ouBAM = pysam.AlignmentFile(ouName, 'wb', template=inBAM)
 
-#fReads = snpMask(snps, inBAM)
-
-#filterBAM(inBAM, ouBAM, fReads)
-
-
-for read in inBAM.fetch(contig):
-    bamL = read.to_dict()
-    mask, m_pos = flagReads(snps, bamL)
-    if mask == True:
-        for p in m_pos:
-            # print('in read %s, position %d' % (bamL['name'], p))
-            t = list(bamL['seq']) ; t[p] = 'N' ; t = "".join(t)
-            bamL['seq'] = t
-        # bamLine --> pysam.AlignedSegment
-        bamL = pysam.AlignedSegment.from_dict(bamL, inBAM.header)
-        ouBAM.write(bamL)
-    else:
-        bamL = pysam.AlignedSegment.from_dict(bamL, inBAM.header)
-        ouBAM.write(bamL)
-
-inBAM.close()
-ouBAM.close()
+processBAM(inBAM, ouBAM, snps, contig)
